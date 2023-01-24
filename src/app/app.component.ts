5d09f2100fb62ff5f373811717c22980e06e4d55
rhare26 <85655946+rhare26@users.noreply.github.com>
2023-01-24 17:43:36 -0500

    updated nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GreatReads';
+  navLinks:any[]= [];
+
+  constructor() {}
+  ngOnInit(): void{
+    this.navLinks = [
+      {
+        label: 'All Books',
+        link: './book',
+        index: 0
+      }, {
+        label: 'All Authors',
+        link: './author',
+        index: 1
+      }, {
+        label: 'My Reads',
+        link: './myread',
+        index: 2
+      },
+    ];
+  }
 }
