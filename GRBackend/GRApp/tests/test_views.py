d919890d042827db42b1b7d18911c83f38036320
rhare26 <85655946+rhare26@users.noreply.github.com>
2023-01-29 10:52:25 -0500

    Basic tests for GET requests for authors

--- GRBackend/GRApp/tests/test_views.py
+++ GRBackend/GRApp/tests/test_views.py
@@ -10,16 +10,30 @@ from ..serializers import AuthorSerializer
 client = Client()
 
 
-class GetAuthor(TestCase):
+class AuthorTest(TestCase):
 
   def setUp(self):
     Author.objects.create(firstName='Alexandre', lastName='Dumas')
     Author.objects.create(firstName='Lemony', lastName='Snicket')
     Author.objects.create(firstName='JK', lastName='Rowling')
 
+    self.base_name = 'book'
+
   def test_get_list_ok(self):
-    response = client.get(reverse('author-list'))
+    response = client.get(reverse(self.base_name + 'list'))
     items = Author.objects.all()
     serializer = AuthorSerializer(items, many=True)
     self.assertEqual(response.data, serializer.data)
     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_get_detail_ok(self):
+    response = client.get(reverse(self.base_name + '-detail', args=[1]))
+    item = Author.objects.get(pk=1)
+    serializer = AuthorSerializer(item)
+    self.assertEqual(response.data, serializer.data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+  def test_get_detail_dne(self):
+    response = client.get(reverse(self.base_name + 'detail', args=[4]))
+    self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
