386fa035ccde175a61a6c76bed34dcf0d9b3d7c6
rhare26 <85655946+rhare26@users.noreply.github.com>
2023-01-29 10:57:26 -0500

    Basic tests for GET requests for authors

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -8,40 +8,39 @@ import { Observable } from 'rxjs';
 
 export class SharedService {
   readonly APIUrl = 'http://127.0.0.1:8000';
-  readonly PhotoUrl = 'http://127.0.0.1:8000/media/';
 
   constructor(private http:HttpClient) { }
 
   getBookList():Observable<any[]>{
-    return this.http.get<any[]>(this.APIUrl + '/books/');
+    return this.http.get<any[]>(this.APIUrl + '/books-list/');
   }
 
   addBook(newBook:any){
-    return this.http.post<any[]>(this.APIUrl + '/books/', newBook);
+    return this.http.post<any[]>(this.APIUrl + '/books-list/', newBook);
   }
 
   updateBook(updatedBook:any){
-    return this.http.put<any[]>(this.APIUrl + '/books/' + updatedBook.id, updatedBook);
+    return this.http.put<any[]>(this.APIUrl + '/book-detail/' + updatedBook.id, updatedBook);
   }
 
   deleteBook(bookToDelete:any){
-    return this.http.delete<any[]>(this.APIUrl + '/books/' + bookToDelete.id, bookToDelete);
+    return this.http.delete<any[]>(this.APIUrl + '/book-detail/' + bookToDelete.id, bookToDelete);
   }
 
   getAuthorList():Observable<any[]>{
-    return this.http.get<any[]>(this.APIUrl + '/authors/');
+    return this.http.get<any[]>(this.APIUrl + '/author-list/');
   }
 
   addAuthor(newAuthor:any){
-    return this.http.post<any[]>(this.APIUrl + '/authors/', newAuthor);
+    return this.http.post<any[]>(this.APIUrl + '/author-list/', newAuthor);
   }
 
   updateAuthor(editedAuthor:any){
-    return this.http.put<any[]>(this.APIUrl + '/authors/' + editedAuthor.id, editedAuthor);
+    return this.http.put<any[]>(this.APIUrl + '/author-detail/' + editedAuthor.id, editedAuthor);
   }
 
   deleteAuthor(authorToDelete:any){
-    return this.http.delete<any[]>(this.APIUrl + '/authors/' + authorToDelete.id, authorToDelete);
+    return this.http.delete<any[]>(this.APIUrl + '/author-detail/' + authorToDelete.id, authorToDelete);
   }
 
 }
