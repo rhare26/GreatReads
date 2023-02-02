5f3fff2d96505b8a1275f815c1fb7ded39fbbb34
rhare26 <85655946+rhare26@users.noreply.github.com>
2023-02-02 07:21:29 -0500

    Starting on nav bar (currently non-functional)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {BrowseComponent} from "./browse/browse.component";
 import {MyReadComponent} from "./my-read/my-read.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
+  {path:'login', component:LoginComponent},
   {path:'browse', component:BrowseComponent},
   {path:'my-reads', component:MyReadComponent},
 ]
