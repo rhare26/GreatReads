c97f872d9a6928ece8976ed170fdd534bb54492d
rhare26 <85655946+rhare26@users.noreply.github.com>
2023-02-06 08:53:18 -0500

    Made form for Register component (not connected to authService or backend yet)

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../_services/auth.service";
+import {MatDialog} from "@angular/material/dialog";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
+  registerForm: any;
+  constructor(private authService:AuthService, public dialog: MatDialog){
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+      password: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    });
+  }
+  submit() {
+    //TODO: add authservice register
+  }
 }
